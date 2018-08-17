f59a3c5c8bb2e49f8abc9f87adbaa2dafde2512b
Thomas Gläßle <t_glaessle@gmx.de>
2018-08-17 05:05:40 +0200

    Change URL/author on PyPI for the time being

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+# encoding: utf-8
 from setuptools import setup
 from setuptools import find_packages
 
 
-version = '0.27.0.dev2'
+version = '0.27.0.dev3'
 
 # Remember to update local-oldest-requirements.txt when changing the minimum
 # acme/certbot version.
@@ -30,9 +31,9 @@ setup(
     version=version,
     description="netcup DNS Authenticator plugin for Certbot",
     long_description=long_description,
-    url='https://github.com/certbot/certbot',
-    author="Certbot Project",
-    author_email='client-dev@letsencrypt.org',
+    url='https://github.com/coldfix/certbot-dns-netcup',
+    author="Thomas Gläßle",
+    author_email='thomas@coldfix.de',
     license='Apache License 2.0',
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
